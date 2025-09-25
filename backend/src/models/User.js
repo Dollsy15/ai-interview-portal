a2655b41db83cdea87986e40847211c976544ef9
Dollsy Rani <dollsyrani155@gmail.com>
2025-09-25 10:36:59 +0530

    Updating files

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema(
     password: { type: String, required: true },
     role: {
       type: String,
-      enum: ["student", "candidate", "admin"],
+      enum: ["student", "candidate", "admin", "user"],
       default: "student",
     },
     scores: {
